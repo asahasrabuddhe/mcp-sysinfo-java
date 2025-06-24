b92cddc808420f0e1380fe8ee287b4fa1c209d36
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2025-06-24 15:21:10 +0530

    initial commit

---
+++ src/test/java/com/technogise/sysinfojava/SysinfoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technogise.sysinfojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysinfoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
